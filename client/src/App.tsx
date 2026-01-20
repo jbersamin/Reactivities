7253beb3bb1b0a92b044afe698cd96012cae5dc7
Jovz <jovvy.bersamin@gmail.com>
2026-01-20 23:54:18 +0800

    end of section 3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,30 @@
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import axios from "axios";
 import { useEffect, useState } from "react"
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    fetch("https://localhost:5001/api/activities")
-    .then(response => response.json())
-    .then(data => setActivities(data));
+    axios.get<Activity[]>("https://localhost:5001/api/activities")
+    .then(response => setActivities(response.data));
+
 
     return () => {}
   }, [])
 
   return (
     <>
-      <h3>Reactivities</h3>
-      <ul>
+      <Typography variant="h3">
+          Reactivities
+      </Typography>
+      <List>
         {activities.map(activity => 
-          <li key={activity.id}>{activity.title}</li>
+          <ListItem key={activity.id}>
+            <ListItemText>{activity.title}</ListItemText>
+          </ListItem>
         )}
-      </ul>
+      </List>
     </>
   )
 }
